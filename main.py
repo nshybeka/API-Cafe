765f5ef5c5b949643ac50c803d99221035354846
Natallia Shybeka <n.shybeka@gmail.com>
2023-07-27 11:01:12 +0400

    Create a /all  route

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-
+import random
 app = Flask(__name__)
 
 # Connect to Database
@@ -23,8 +23,22 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        # 1 Method:
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+        # 2 Method:
+        # dictionary = {}
 
-with app.create_contect():
+        # # Loop through each column in the data record
+        # for column in self.__table__.columns:
+        #     # Create a new dictionary entry;
+        #     # where the key is the name of the column
+        #     # and the value is the value of the column
+        #     dictionary[column.name] = getattr(self, column.name)
+        # return dictionary
+
+
+with app.app_context():
     db.create_all()
 
 
@@ -34,14 +48,37 @@ def home():
 
 
 # HTTP GET - Read
-@app.route("/random", method=["GET"])
-def get_random_cafe():
-    pass
+# @app.route("/random", method=["GET"])
+# def get_random_cafe():
+#     pass
 
 
 @app.route("/random")
 def get_random_cafe():
-    pass
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(cafe=random_cafe.to_dict())
+    # return jsonify(cafe={
+    #     "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #     "seats": random_cafe.seats,
+    #     "has_toilet": random_cafe.has_toilet,
+    #     "has_wifi": random_cafe.has_wifi,
+    #     "has_sockets": random_cafe.has_sockets,
+    #     "can_take_calls": random_cafe.can_take_calls,
+    #     "coffee_price": random_cafe.coffee_price,
+    # })
+
+
+@app.route("/all")
+def get_all_cafes():
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafes = result.scalars().all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
 
 ## HTTP POST - Create Record
 
@@ -51,4 +88,4 @@ def get_random_cafe():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
